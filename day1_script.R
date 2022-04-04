67e0632f716e45764f3ef98d517ef17f17504158
Knkzw <kn540@georgetown.edu>
2022-04-04 17:30:01 -0400

    file change

---
+++ day1_script.R
@@ -0,0 +1,20 @@
+# Making a git repo example
+# Using shark data
+# Kai Nakazawa
+# April 4 2022
+
+dolphins <- read.csv("dolphins.csv")
+shark_bites <- read.csv("shark_bites.csv")
+sightings <- read.csv("sightings.csv")
+
+ind_group_sizes <- aggregate(Group.Size~Dolphin.ID, 
+                             
+                             data = sightings,
+                             median)
+counts <- table(sightings$Dolphin.ID)
+
+ind_group_sizes$n_sightings <- counts[match(ind_group_sizes$Dolphin.ID , names(counts))]
+
+ind_group_sizes$Sex <- dolphins$Sex[match(ind_group_sizes$Dolphin.ID, dolphins$Dolphin.ID
+                                          )]
+
